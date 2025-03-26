10547c12f55ae753eb4ee5cf2094a2b146276f83
Fabian Istvan <i.fabian@mito.hu>
2025-03-26 14:56:33 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "10px", color: "purple" }}>
+      Hi there hello, my name is Norbi
+    </div>
+  );
+}
