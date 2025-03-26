3a4ac2144299973fdf36402d2f8e13cfd1e021ce
Fabian Istvan <i.fabian@mito.hu>
2025-03-26 15:12:06 +0100

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div style={{ padding: "10px", color: "purple" }}>
-      Hi there hello, my name is Norbi
+      Hi there hello, my name is Norbi, this is the test branch hurraaaaay
     </div>
   );
 }
